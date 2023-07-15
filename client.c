91e092899a2756e875b9c9186d5bc2249954e094
Alfredo A. Hernandez I <alfredo.hernandez@ug.uchile.cl>
2023-07-15 09:03:53 +0100

    advances

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: aantonio <aantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 13:25:38 by aantonio          #+#    #+#             */
-/*   Updated: 2023/06/27 13:40:54 by aantonio         ###   ########.fr       */
+/*   Updated: 2023/07/14 17:14:06 by aantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,13 @@ int	main(int argc, char *argv[] )
 {
 	int	server_pid;
 
-	if (argc > 2)
+	if (argc != 3)
 	{
-		printf("One argument expected!");
+		printf("Only one argument expected!");
+		exit(-1);
 	}
 	server_pid = atoi(argv[1]);
 	kill(server_pid, SIGUSR1);
+	printf("%s\n",argv[2]);
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,29 +6,85 @@
 /*   By: aantonio <aantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 12:33:50 by aantonio          #+#    #+#             */
-/*   Updated: 2023/06/27 13:38:39 by aantonio         ###   ########.fr       */
+/*   Updated: 2023/07/14 17:52:44 by aantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 void	my_handler(int signum)
 {
+	
+		write(1, "Received signal signal()!\n", 27);
 	if (signum == SIGUSR1)
 	{
-		printf("Received SIGUSR1!\n");
+		write(1, "Received SIGUSR1!\n", 19);
+		exit(0);
 	}
 }
 
+
+void	handler(int signum, siginfo_t *info, void *ucontext)
+{
+	// write(1, "Received signal sigaction()!\n", 30);
+	if (signum == SIGUSR1)
+	{
+		write(1, "Received SIGUSR1!\n", 19);
+		struct sigaction	sa;
+
+		// text = malloc(1);
+		sigemptyset(&sa.sa_mask);
+		sa.sa_handler = handler;
+		sa.sa_flags = SA_NODEFER|SA_RESETHAND;
+		sigaction(SIGUSR1, &sa, NULL);
+		pause();
+		// text
+	}
+}
+
+// char*	text;
+
 int	main(void)
 {
-	int	server_pid;
+	int					server_pid;
+	struct sigaction	sa;
 
+	// text = malloc(1);
+	sigemptyset(&sa.sa_mask);
+	sa.sa_handler = handler;
+	sa.sa_flags = SA_NODEFER|SA_RESETHAND;
 	server_pid = getpid();
 	printf("Server PID: %d\n", server_pid);
-	pause();
-	signal(SIGUSR1, my_handler);
+	sigaction(SIGUSR1, &sa, NULL);
+	// if (not finished text)
+		pause();
+	// write(text)
+
+	// while (1)
+	// {
+	// 	sigaction(SIGUSR1, &new_action, NULL);
+	// 	sleep();
+	// }
+	// free(text);
 	return (0);
 }
+
+
+// int	main(void)
+// {
+// 	int	server_pid;
+// 	struct sigaction new_action;
+
+// 	new_action.sa_handler = handler;
+// 	server_pid = getpid();
+// 	printf("Server PID: %d\n", server_pid);
+// 	sigaction (SIGUSR1, &new_action, NULL);
+// 	// while(1)
+// 	// 	continue;
+// 	// signal(SIGUSR1, my_handler);
+// 	pause();
+// 	return (0);
+// }
